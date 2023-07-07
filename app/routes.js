80c50e26c94c0ac52740568816db1dc0c5288f50
Darren Courtney <113589359+dcdfe@users.noreply.github.com>
2023-07-07 14:31:31 +0100

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'destination-answer'
+router.post('/destination-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'destination'
+  var destination = req.session.data['destination']
+
+  // Check whether the variable matches a condition
+  if (destination == "moon" || destination == "mars") {
+    // Send user to next page
+    res.redirect('/full-name')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
